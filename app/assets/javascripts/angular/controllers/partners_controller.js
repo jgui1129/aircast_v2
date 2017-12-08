57104d7ad78a76df7f25bfb3a3f556d742861d8e
Jeric <jgui1129.jg@gmail.com>
2017-12-09 03:08:11 +0800

    added locatios per site

---
+++ app/assets/javascripts/angular/controllers/partners_controller.js
@@ -0,0 +1,9 @@
+angular.module('Aircast.controllers')
+  .controller('PartnerController',
+  ['$scope', '$state', 'RpiService', 'AuthService',
+    function($scope, $state, RpiService, AuthService) {
+
+    	
+
+
+}]);
